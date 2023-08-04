56b12aa35b96e622c8df935f6b15a75f0aed2c10
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-08-04 11:52:18 +0300

    feat: классы Item и ItemDto

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemDto {
+    Long id;
+    Long ownerId;
+    String name;
+    String description;
+    Boolean available;
 }
--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,21 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.user.User;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Item {
+    Long id;
+    User owner;
+    String name;
+    String description;
+    Boolean available;
 }
