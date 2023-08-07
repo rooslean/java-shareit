33c9bcbf6961180eba92bb1cfcf0c28dd2242aeb
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-08-07 23:49:08 +0300

    perf: логирование стактрейса исключений

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -14,28 +14,28 @@ public class ErrorHandler {
     @ExceptionHandler({ObjectNotFoundException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleObjectNotFound(final RuntimeException e) {
-        log.info(e.getMessage());
+        log.info(e.getMessage(), e);
         return new ErrorResponse("Объект не найден", e.getMessage());
     }
 
     @ExceptionHandler({ObjectAlreadyExistsException.class})
     @ResponseStatus(HttpStatus.CONFLICT)
     public ErrorResponse handleObjectAlreadyExists(final RuntimeException e) {
-        log.info(e.getMessage());
+        log.info(e.getMessage(), e);
         return new ErrorResponse("Объект уже существует", e.getMessage());
     }
 
     @ExceptionHandler({ObjectNotValidException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleObjectNotValid(final RuntimeException e) {
-        log.info(e.getMessage());
+        log.info(e.getMessage(), e);
         return new ErrorResponse("Невалидные данные", e.getMessage());
     }
 
     @ExceptionHandler({NoRightsForUpdateException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public ErrorResponse handleNoRightsForUpdate(final RuntimeException e) {
-        log.info(e.getMessage());
+        log.info(e.getMessage(), e);
         return new ErrorResponse("Нет доступа", e.getMessage());
     }
 }
