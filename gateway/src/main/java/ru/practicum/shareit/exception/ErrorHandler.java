256ffc62f867cc62c516ef04cae0ed2968cb7dfa
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-10-08 11:02:49 +0300

    perf: обработчик ошибок валидации

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.shareit.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -22,5 +23,12 @@ public class ErrorHandler {
     public ErrorResponse handleGeneralError(final Throwable e) {
         log.warn(e.getMessage());
         return new ErrorResponse(e.getMessage(), "");
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationError(final MethodArgumentNotValidException e) {
+        log.warn(e.getMessage());
+        return new ErrorResponse("Ошибка валидации", e.getMessage());
     }
 }
