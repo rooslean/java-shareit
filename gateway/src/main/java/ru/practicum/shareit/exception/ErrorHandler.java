b2f19b5995878599d0291b8d8dc99bcaab205c08
Ruslan Dusaev <rusyadusaev@gmail.com>
2023-10-08 10:57:47 +0300

    perf: новый обработчик ошибок

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -16,4 +16,11 @@ public class ErrorHandler {
         log.warn(e.getMessage());
         return new ErrorResponse(e.getMessage(), "");
     }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleGeneralError(final Throwable e) {
+        log.warn(e.getMessage());
+        return new ErrorResponse(e.getMessage(), "");
+    }
 }
